4662aff987a87fe0f28d2aab22fdfb0ec181d617
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2023-09-26 08:46:25 -0500

    Ajustes 7

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -13,7 +13,7 @@
 
     figure.mb-5(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/Q_GZXM9wqUg?si=t_E9JHjRuSm7xM2m" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     p Para la elaboración de este componente, se abordaron varios autores conocidos en #[strong procesos de importación], de quienes se han citado y referenciado conceptos y ejemplos para los fines educativos de esta materia, en el entendido de que el conocimiento es social y, por lo tanto, es para ser usado por quienes necesitan adquirirlo. Se espera que este documento sea útil para todos aquellos, aprendices y lectores en general, que estén interesados en acercarse a asuntos básicos de la #[strong coordinación de procesos logísticos].
     
